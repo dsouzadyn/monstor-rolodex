bcd45f8c146f1f01c671389ba049c9c4ee059219
Dylan Dsouza <dsouzadyn@gmail.com>
2020-01-30 11:56:26 +0530

    Updates

---
+++ precache-manifest.b759172ad1abbf30a443d4efa400e1bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "838557124eb76c84a5dc284202f3f7dd",
+    "url": "/monstor-rolodex/index.html"
+  },
+  {
+    "revision": "67b325fe851bdfaf3196",
+    "url": "/monstor-rolodex/static/css/main.2d5ccfe4.chunk.css"
+  },
+  {
+    "revision": "0f6228d43e8328d08235",
+    "url": "/monstor-rolodex/static/js/2.5815e45a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monstor-rolodex/static/js/2.5815e45a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "67b325fe851bdfaf3196",
+    "url": "/monstor-rolodex/static/js/main.2cc0c4b1.chunk.js"
+  },
+  {
+    "revision": "2acf591adfacf461840e",
+    "url": "/monstor-rolodex/static/js/runtime-main.081942ec.js"
+  }
+]);
